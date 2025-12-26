e97db87acd7589a433e074f15b92a2d895df9800
Calor Rola <carloneu94@gmx.de>
2025-12-26 14:05:25 +0100

    Fix indentation in cloudinary configuration

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ cloudinary.config(
     cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
     api_key=os.environ.get('CLOUDINARY_API_KEY'),
     api_secret=os.environ.get('CLOUDINARY_API_SECRET')
-
+)
     some_function(
     arg1,
     arg2
