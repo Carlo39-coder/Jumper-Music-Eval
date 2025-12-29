0f63e55c9a6ade5563a21ec63b5919a68a8f442e
Calor Rola <carloneu94@gmx.de>
2025-12-29 17:07:11 +0100

    Update app.py
    Signed-off-by: Calor Rola <carloneu94@gmx.de>

--- app.py
+++ app.py
@@ -229,8 +229,8 @@ def rate(track_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('FLASK_DEBUG', 'False') == 'True')
-    import markdown
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 @app.route('/kriterien')
 def kriterien():
