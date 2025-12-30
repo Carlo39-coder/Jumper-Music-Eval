4cef87c794385fbccabee3d8cde50dc30aaa3392
Calor Rola <carloneu94@gmx.de>
2025-12-30 23:57:59 +0100

    Update app.py
    Signed-off-by: Calor Rola <carloneu94@gmx.de>

--- app.py
+++ app.py
@@ -66,10 +66,6 @@ class Track(db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 with app.app_context():
     db.create_all()
 
