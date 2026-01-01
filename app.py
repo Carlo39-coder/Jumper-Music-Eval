2a38a645eca887da1997ed1084e86042f3b971f4
Calor Rola <carloneu94@gmx.de>
2026-01-01 01:59:08 +0100

    Update app.py
    Signed-off-by: Calor Rola <carloneu94@gmx.de>

--- app.py
+++ app.py
@@ -220,7 +220,3 @@ if __name__ == '__main__':
         db.create_all()  # Erstellt Tabellen, falls noch nicht vorhanden
     app.run(debug=True)
     
-@app.route('/init-db')
-def init_db():
-    db.create_all()
-    return "Datenbank-Tabellen erfolgreich angelegt! Du kannst diese Route jetzt wieder löschen."
