362095d1b8519f4ca115afef35b7b07b1bd0bfdd
Calor Rola <carloneu94@gmx.de>
2026-01-01 01:49:19 +0100

    Update app.py
    Signed-off-by: Calor Rola <carloneu94@gmx.de>

--- app.py
+++ app.py
@@ -212,7 +212,11 @@ def rate(track_id):
 
     return render_template('rate.html', track=track)
 
-
+@app.route('/init-db')
+def init_db():
+    db.create_all()
+    return "Datenbank-Tabellen erfolgreich angelegt! Du kannst diese Route jetzt wieder löschen."
+    
 # Nur für lokales Testen: Tabellen anlegen
 if __name__ == '__main__':
     with app.app_context():
