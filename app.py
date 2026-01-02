85c4fb2fd6aae1e3d38ecf9929f8bf3e61d559db
Calor Rola <carloneu94@gmx.de>
2026-01-02 08:38:00 +0100

    Update app.py
    Signed-off-by: Calor Rola <carloneu94@gmx.de>

--- app.py
+++ app.py
@@ -227,5 +227,6 @@ def kriterien_theorie():
 
 # --------------------- Start der App ---------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
     
