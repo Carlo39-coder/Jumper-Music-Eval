e0a1de3b858e6371303efa761ee0a53549a8ef0a
Calor Rola <carloneu94@gmx.de>
2026-01-19 23:55:46 +0100

    Update app.py
    Signed-off-by: Calor Rola <carloneu94@gmx.de>

--- app.py
+++ app.py
@@ -251,9 +251,7 @@ def rate(track_id):
 
     return render_template('rate.html', track=track, kriterien=KRITERIEN)
 
-@app.route('/kriterien_theorie')
-def kriterien_theorie():
-    return render_template('kriterien_theorie.html')
+
 
 # Starte die App
 if __name__ == '__main__':
