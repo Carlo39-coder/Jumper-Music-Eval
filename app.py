9d054071496a2840b54999feb52f6ebdeeedf116
Calor Rola <carloneu94@gmx.de>
2026-01-21 00:01:40 +0100

    Update app.py
    Signed-off-by: Calor Rola <carloneu94@gmx.de>

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     alter = db.Column(db.Integer, nullable=False)
     is_mentor = db.Column(db.Boolean, default=False)
     is_admin = db.Column(db.Boolean, default=False)
