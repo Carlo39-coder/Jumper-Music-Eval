afe09b0ac0a5f91d7fc659a181c1d50a1a5d1717
Calor Rola <carloneu94@gmx.de>
2026-01-22 18:34:19 +0100

    Update app.py
    Signed-off-by: Calor Rola <carloneu94@gmx.de>

--- app.py
+++ app.py
@@ -123,7 +123,8 @@ class User(UserMixin, db.Model):
     is_admin = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    # Immer dieselbe Methode – aktuell empfohlen und sicher
+    self.password_hash = generate_password_hash(password, method='pbkdf2:sha256:600000')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
