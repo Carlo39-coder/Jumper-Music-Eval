0a3c5179729a95ee9254f9339ed85b2778b939f0
Calor Rola <carloneu94@gmx.de>
2026-01-23 23:17:50 +0100

    Update app.py
    Signed-off-by: Calor Rola <carloneu94@gmx.de>

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import json
 import logging
 from datetime import datetime
-
 from flask import Flask, render_template, request, redirect, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
